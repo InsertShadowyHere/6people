ed69cc01b5f642211bf86a9b211c97e1f36a4e54
InsertShadowyHere <raphaelquarles@gmail.com>
2025-06-12 15:41:17 -0400

    added basic interaction

---
+++ objects/gui.gd
@@ -0,0 +1,13 @@
+extends Control
+
+var interact_label
+
+func _ready():
+	interact_label = $InteractLabel
+	interact_label.visible = false
+	
+func _on_player_looking_at_stuff(stuff: Variant) -> void:
+	interact_label.visible = true
+
+func _on_player_not_looking_at_stuff() -> void:
+	interact_label.visible = false
--- scripts/player.gd
+++ scripts/player.gd
@@ -1,11 +1,17 @@
 # In your CharacterBody3D script
 extends CharacterBody3D
 
+signal looking_at_stuff(stuff)
+signal not_looking_at_stuff
+
 @export var mouse_sensitivity = 0.002 # radians/pixel
 @export var speed = 20
 @export var jump_strength = 20
 @export var gravity = 1
 @export var range = 100
+
+var viewed_object
+var interactible_displayed = false
 
 var pi_over_two = 3.1415/2
 
@@ -22,12 +28,13 @@ var left
 var test = false
 
 @onready var camera = $Camera
-@onready var raycast = $Camera/Raycast
+@onready var hitcaster = $Camera/Hitcaster
+@onready var sightcaster = $Camera/Sightcaster
 
 func _ready():
 	camera.fov = fov
-	raycast.scale *= range #adjusting because normal raycast scale is WAY too low
-
+	hitcaster.scale *= range #adjusting because normal raycast scale is WAY too low
+	sightcaster.scale *= 2
 
 func _input(event):
 	if event is InputEventMouseMotion:
@@ -51,12 +58,22 @@ func _input(event):
 		else:
 			zoomed = true
 			camera.fov = 30
-	elif Input.is_action_just_pressed("fire") and raycast.is_colliding():
-		raycast.get_collider().queue_free()
+	elif Input.is_action_just_pressed("fire") and hitcaster.is_colliding():
+		hitcaster.get_collider().queue_free()
+	elif Input.is_action_just_pressed("interact") and sightcaster.is_colliding():
+		if sightcaster.get_collider().has_method("interact"):
+			sightcaster.get_collider().interact()
 	
 func _process(delta: float) -> void:
-	pass
-		
+	if interactible_displayed == false:
+		if sightcaster.is_colliding():
+			viewed_object = sightcaster.get_collider()
+			if viewed_object.is_in_group("interactible"):
+				emit_signal("looking_at_stuff", viewed_object)
+				interactible_displayed = true
+	elif interactible_displayed and not sightcaster.is_colliding():
+		interactible_displayed = false
+		emit_signal("not_looking_at_stuff")
 
 func _physics_process(delta):
 	velocity = Vector3(0, velocity[1], 0)
@@ -73,8 +90,6 @@ func _physics_process(delta):
 		velocity -= left
 	if Input.is_action_pressed("right"):
 		velocity += left
-	
-	
 
 	preserve_y_velocity = velocity[1]
 	velocity[1] = 0
@@ -85,5 +100,8 @@ func _physics_process(delta):
 			velocity[1] = jump_strength
 	
 	velocity[1] -= gravity
-
 	move_and_slide()
+
+
+func _on_looking_at_stuff(stuff: Variant) -> void:
+	pass # Replace with function body.
