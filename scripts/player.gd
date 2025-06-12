55bbaddaa99dda25f992647de5e0816028fef62f
InsertShadowyHere <raphaelquarles@gmail.com>
2025-06-12 14:47:10 -0400

    added zoom and refactored

--- scripts/player.gd
+++ scripts/player.gd
@@ -5,10 +5,13 @@ extends CharacterBody3D
 @export var speed = 20
 @export var jump_strength = 20
 @export var gravity = 1
+@export var range = 100
 
 var pi_over_two = 3.1415/2
 
 var camera_view = 1
+var zoomed = false
+var fov = 90
 
 var vertical_angle = 0.0
 
@@ -22,7 +25,9 @@ var test = false
 @onready var raycast = $Camera/Raycast
 
 func _ready():
-	raycast.scale *= 10 #adjusting because normal raycast scale is WAY too low
+	camera.fov = fov
+	raycast.scale *= range #adjusting because normal raycast scale is WAY too low
+
 
 func _input(event):
 	if event is InputEventMouseMotion:
@@ -30,13 +35,28 @@ func _input(event):
 		if vertical_angle - event.relative.y * mouse_sensitivity == clamp(vertical_angle - event.relative.y * mouse_sensitivity, -pi_over_two, pi_over_two):
 			camera.rotate_x(-event.relative.y * mouse_sensitivity)
 			vertical_angle -= event.relative.y * mouse_sensitivity
-		
-
-func _process(delta: float) -> void:
-	if Input.is_action_pressed("fire") and raycast.is_colliding():
-		print(raycast.get_collider())
+	elif Input.is_action_just_pressed("switch_camera"):
+		if camera_view == 1:
+			camera_view = 2
+			camera.position[2] += 5
+			camera.position[1] += 2
+		else:
+			camera_view = 1
+			camera.position[2] -= 5
+			camera.position[1] -= 2
+	elif Input.is_action_just_pressed("zoom"):
+		if zoomed == true:
+			zoomed = false
+			camera.fov = fov
+		else:
+			zoomed = true
+			camera.fov = 30
+	elif Input.is_action_just_pressed("fire") and raycast.is_colliding():
 		raycast.get_collider().queue_free()
-		print('freeing queue')
+	
+func _process(delta: float) -> void:
+	pass
+		
 
 func _physics_process(delta):
 	velocity = Vector3(0, velocity[1], 0)
@@ -54,16 +74,7 @@ func _physics_process(delta):
 	if Input.is_action_pressed("right"):
 		velocity += left
 	
-	# for now this is just a debug thing
-	if Input.is_action_just_pressed("switch_camera"):
-		if camera_view == 1:
-			camera_view = 2
-			camera.position[2] += 5
-			camera.position[1] += 2
-		else:
-			camera_view = 1
-			camera.position[2] -= 5
-			camera.position[1] -= 2
+	
 
 	preserve_y_velocity = velocity[1]
 	velocity[1] = 0
